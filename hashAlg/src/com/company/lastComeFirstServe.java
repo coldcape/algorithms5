4587b6f0e9dc4f6f3ef5367e9d02a4e93904fe9e
Raptor Tech <44273999+coldcape@users.noreply.github.com>
2022-04-07 18:13:10 +0200

    Bib bab bop

---
+++ hashAlg/src/com/company/lastComeFirstServe.java
@@ -0,0 +1,168 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class lastComeFirstServe {
+    // Hashlengde
+    private int hashLengde;
+
+    // Hashtabell
+    private final String[] hashTabell;
+
+    // Antall elementer lagret i tabellen
+    private int n;
+
+    // Antall probes ved innsetting
+    private int antProbes;
+
+    // KonstruktÃ¸r
+    // Sjekker ikke for fornuftig verdi av hashlengden
+    //
+    public hashLinear(int lengde) {
+        hashLengde = lengde;
+        hashTabell = new String[lengde];
+        n = 0;
+        antProbes = 0;
+    }
+
+    // Returnerer load factor
+    public float loadFactor() {
+        return ((float) n) / hashLengde;
+    }
+
+    // Returnerer antall data i tabellen
+    public int antData() {
+        return n;
+    }
+
+    // Returnerer antall probes ved innsetting
+    public int antProbes() {
+        return antProbes;
+    }
+
+    // Hashfunksjon
+    int hash(String S) {
+        int h = Math.abs(S.hashCode());
+        return h % hashLengde;
+    }
+
+    // Innsetting av tekststreng med lineÃ¦r probing
+    // Avbryter med feilmelding hvis ledig plass ikke finnes
+    //
+    void insert(String S) {
+        // Beregner hashverdien
+        int h = hash(S);
+
+        // LineÃ¦r probing
+        int neste = h;
+
+        while (hashTabell[neste] != null) {
+            // Ny probe
+            antProbes++;
+
+            // Denne indeksen er opptatt, prÃ¸ver neste
+            neste++;
+
+            // Wrap-around
+            if (neste >= hashLengde)
+                neste = 0;
+
+            // Hvis vi er kommet tilbake til opprinnelig hashverdi, er
+            // tabellen full og vi gir opp (her ville man normalt
+            // doblet lengden pÃ¥ hashtabellen og gjort en rehashing)
+            if (neste == h) {
+                System.err.println("\nHashtabell full, avbryter");
+                System.exit(0);
+            }
+        }
+
+        // Lagrer tekststrengen pÃ¥ funnet indeks
+        hashTabell[neste] = S;
+
+        // Ã˜ker antall elementer som er lagret
+        n++;
+    }
+
+    // SÃ¸king etter tekststreng med lineÃ¦r probing
+    // Returnerer true hvis strengen er lagret, false ellers
+    //
+    boolean search(String S) {
+        // Beregner hashverdien
+        int h = hash(S);
+
+        // LineÃ¦r probing
+        int neste = h;
+
+        while (hashTabell[neste] != null) {
+            // Har vi funnet tekststrengen?
+            if (hashTabell[neste].compareTo(S) == 0)
+                return true;
+
+            // PrÃ¸ver neste mulige
+            neste++;
+
+            // Wrap-around
+            if (neste >= hashLengde)
+                neste = 0;
+
+            // Hvis vi er kommet tilbake til opprinnelig hashverdi,
+            // finnes ikke strengen i tabellen
+            if (neste == h)
+                return false;
+        }
+
+        // Finner ikke strengen, har kommet til en probe som er null
+        return false;
+    }
+
+    // Enkelt testprogram:
+    //
+    // * Hashlengde gis som input pÃ¥ kommandolinjen
+    //
+    // * Leser tekststrenger linje for linje fra standard input
+    //   og lagrer dem i hashtabellen
+    //
+    // * Skriver ut litt statistikk etter innsetting
+    //
+    // * Tester om sÃ¸k fungerer for et par konstante verdier
+    //
+    public static void main(String[] argv) throws FileNotFoundException {
+        // Hashlengde leses fra kommandolinjen
+        int hashLengde = 0;
+        File file = new File("../hashAlg/src/com/company/car.txt");
+        Scanner input = new Scanner(file);
+
+        hashLinear hL;
+        hashLengde = input.nextInt();
+        hL = new hashLinear(hashLengde);
+
+        System.out.println(hashLengde);
+        while (input.hasNext()) {
+            String linje = input.nextLine();
+            hL.insert(linje);
+
+        }
+
+
+        // Skriver ut hashlengde, antall data lest, antall kollisjoner
+        // og load factor
+        System.out.println("Hashlengde  : " + hashLengde);
+        System.out.println("Elementer   : " + hL.antData());
+        System.out.printf("Load factor : %5.3f\n", hL.loadFactor());
+        System.out.println("Probes      : " + hL.antProbes());
+
+        // Et par enkle sÃ¸k
+        String S = "Volkswagen Karmann Ghia";
+        if (hL.search(S))
+            System.out.println("\"" + S + "\"" + " finnes i hashtabellen");
+        S = "Il Tempo Gigante";
+        if (!hL.search(S))
+            System.out.println("\"" + S + "\"" + " finnes ikke i hashtabellen");
+
+
+    }
+
+
+}
